424efa11f98572ec92cb368d1f6cc61aa50d3d4b
benjamin chambon <benjamin.chambon@epita.fr>
2017-06-29 14:45:16 +0200

    remove DataAccess and managers

--- src/AdSite/AdSiteBundle/Controller/LoginController.php
+++ src/AdSite/AdSiteBundle/Controller/LoginController.php
@@ -3,32 +3,31 @@
 namespace AdSite\AdSiteBundle\Controller;
 
 use AdSite\AdSiteBundle\Entity\Connexion;
-use AdSite\AdSiteBundle\Entity\Produits;
-use AdSite\AdSiteBundle\Repository\ArticlesRepository;
 use AdSite\AdSiteBundle\Manager\ArticlesManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AdSite\AdSiteBundle\Form\LoginFormType;
 use Symfony\Component\HttpFoundation\Request;
 
 
-class ConnexionController extends Controller
+class LoginController extends Controller
 {
-    public function connexionFormulaireAction(Request $request)
+    public function loginAction(Request $request)
     {
         $connexion = new Connexion();
         $form = $this->createForm(LoginFormType::class, $connexion);
         $form->handleRequest($request);
 
         $pseudo = '';
-        $produits = [];
+        $articles = [];
+
         if ($form->isValid()) {
             $article_access = new ArticlesManager($this->getDoctrine()->getManager());
             $pseudo = $form->get('pseudo')->getData();
             $article_access->insertDataBase($pseudo);
-            $produits = $article_access->getData();
+            $articles = $article_access->getData();
         }
 
-        return $this->render('AdSiteBundle:Default:index.html.twig', array('form' => $form->createView(), 'pseudo' => $pseudo, 'produits' => $produits));
+        return $this->render('AdSiteBundle:Default:index.html.twig', array('form' => $form->createView(), 'pseudo' => $pseudo, 'articles' => $articles));
     }
 
 }
--- src/AdSite/AdSiteBundle/Manager/ArticlesManager.php
+++ src/AdSite/AdSiteBundle/Manager/ArticlesManager.php
@@ -20,20 +20,16 @@ class ArticlesManager
 
     public function insertDataBase($data)
     {
+
         //insertion dans la base
         $article = new Articles();
-        //  $produits->setCategorie($data);
         $article->setTitle('Tomate');
         $article->setDescription('blabla');
         $article->setCategory('voiture');
-        $dt = new DateTime();
         $article->setPlace("Sivry");
-        $date = new DateTime('2000-01-01');
+        $date = new \DateTime('today');
         $article->setDate($date);
         $article->setPrice('1');
-        // $produits->setDisponible('1');
-        // $produits->setImage('https://www.google.fr/search?q=norv%C3%A8ge&rlz=1C1CHBF_frFR718FR718&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj8_em4iMfUAhXpJ8AKHWcpD10Q_AUICygC&biw=1366&bih=662#imgrc=vfJSMm3s6dpq9M:');
-        //$produits->setTva('19.6');
 
         $this->em->persist($article);
         $this->em->flush();
