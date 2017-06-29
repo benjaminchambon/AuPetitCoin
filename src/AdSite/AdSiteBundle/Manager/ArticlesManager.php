e43cca333a7f1485cb1fe376b35b619f81909d1a
benjamin chambon <benjamin.chambon@epita.fr>
2017-06-29 17:34:20 +0200

    add insert/delete/update/get article in manager/repository

--- src/AdSite/AdSiteBundle/Manager/ArticlesManager.php
+++ src/AdSite/AdSiteBundle/Manager/ArticlesManager.php
@@ -6,40 +6,77 @@
  * Date: 29/06/2017
  * Time: 12:02
  */
+
 namespace AdSite\AdSiteBundle\Manager;
+
 use AdSite\AdSiteBundle\Entity\Articles;
-use Symfony\Component\Validator\Constraints\DateTime;
 
 class ArticlesManager
 {
     private $em;
+    private $articleRepo;
+
     public function __construct($em)
     {
         $this->em = $em;
+        $this->articleRepo = $this->em->getRepository('AdSiteBundle:Articles');
     }
 
-    public function insertDataBase($data)
+    public function insertArticle($data)
     {
-
-        //insertion dans la base
         $article = new Articles();
-        $article->setTitle('Tomate');
-        $article->setDescription('blabla');
-        $article->setCategory('voiture');
-        $article->setPlace("Sivry");
-        $date = new \DateTime('today');
-        $article->setDate($date);
-        $article->setPrice('1');
+        $article->setTitle($data->getTitle());
+        $article->setCategory($data->getCategory());
+        $article->setPlace($data->getPlace());
+        $article->setDate(new \DateTime('today'));
+        $article->setPrice($data->getPrice());
+        $article->setDescription($data->getDescription());
 
         $this->em->persist($article);
         $this->em->flush();
-
-        //RECUPERER DES DONNES DANS LA
-        // $produits = $em->getRepository('EcommerceBundle:Produits')->findAll();
     }
 
-    public function getData(){
-        $produits = $this->em->getRepository('AdSiteBundle:Articles')->findAll();
-        return $produits;
+    public function deleteArticle($id)
+    {
+        $post = $this->articleRepo->find($id);
+        if (!$post) {
+            return false;
+        }
+        $this->em->remove($post);
+        $this->em->flush();
+        return true;
+    }
+
+    public function updateArticle($id, $data)
+    {
+        $post = $this->articleRepo->find($id);
+        if (!$post) {
+            return false;
+        }
+        if ($data->getTitle() != null)
+            $post->setTitle($data->getTitle());
+        if ($data->getCategory() != null)
+            $post->setCategory($data->getCategory());
+        if ($data->getPlace() != null)
+            $post->setPlace($data->getPlace());
+        if ($data->getDate() != null)
+            $post->setDate($data->getDate());
+        if ($data->getPrice() != null)
+            $post->setPrice($data->getPrice());
+        if ($data->getDescription() != null)
+            $post->setDescription($data->getDescription());
+
+        $this->em->flush();
+        return true;
+    }
+
+    public function getAllArticles()
+    {
+        return $this->articleRepo->findAll();
+    }
+
+    public function getArticlesByCategory($category)
+    {
+        return $this->articleRepo->getArticlesByCategory($category);
     }
 }
