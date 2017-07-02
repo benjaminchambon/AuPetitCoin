e7a5e92775848a29a64bae1465e4eba9a20e3a09
benjamin chambon <benjamin.chambon@epita.fr>
2017-07-02 16:09:04 +0200

    handle many word in research

--- src/AdSite/AdSiteBundle/Repository/ArticlesRepository.php
+++ src/AdSite/AdSiteBundle/Repository/ArticlesRepository.php
@@ -34,22 +34,45 @@ class ArticlesRepository extends \Doctrine\ORM\EntityRepository
 
     public function getArticlesFiltered($keywords, $category, $city)
     {
+        $keywords_tab = explode(' ', $keywords);
+        $array_tmp = array(0 => 'vvvvvvvvv', 1=> 'vvvvvvvvvv');
+        for ($i = 4 - count($keywords_tab); $i >= count($keywords_tab); $i--) {
+            $keywords_tab = array_merge($keywords_tab, $array_tmp);
+        }
+
+        print_r($keywords_tab);
+
         if (strcmp($category, "Tous") == 0) {
             $articles = $this->getEntityManager()->createQuery('
         SELECT a FROM AdSiteBundle:Articles a
-        WHERE (a.description LIKE :keywords OR
-        a.title LIKE :keywords) AND
+        WHERE (a.description LIKE :keywords0 OR
+        a.title LIKE :keywords0 OR 
+        a.description LIKE :keywords1 OR
+        a.title LIKE :keywords1 OR
+        a.description LIKE :keywords2 OR
+        a.title LIKE :keywords2) AND
         ( a.place = :city OR :city IS NULL )
-        ')->setParameters(array('keywords' => '%' . $keywords . '%', 'city' => $city))->getResult();
+        ')->setParameters(array('keywords0' => '%' . $keywords_tab[0] . '%',
+                'keywords1' => '%' . $keywords_tab[1] . '%',
+                'keywords2' => '%' . $keywords_tab[2] . '%',
+                'city' => $city))->getResult();
         }
         else{
             $articles = $this->getEntityManager()->createQuery('
         SELECT a FROM AdSiteBundle:Articles a
-        WHERE (a.description LIKE :keywords OR
-        a.title LIKE :keywords) AND 
+        WHERE (a.description LIKE :keywords0 OR
+        a.title LIKE :keywords0 OR 
+        a.description LIKE :keywords1 OR
+        a.title LIKE :keywords1 OR
+        a.description LIKE :keywords2 OR
+        a.title LIKE :keywords2) AND 
         (a.place = :city OR :city IS NULL ) AND
         a.category = :category
-        ')->setParameters(array('keywords' => '%' . $keywords . '%', 'category' => $category, 'city' => $city))->getResult();
+        ')->setParameters(array('keywords0' => '%' . $keywords_tab[0] . '%',
+                'keywords1' => '%' . $keywords_tab[1] . '%',
+                'keywords2' => '%' . $keywords_tab[2] . '%',
+                'category' => $category,
+                'city' => $city))->getResult();
         }
 
         return $articles;
